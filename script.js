11d1ad9a863c104e024bff6f411ca019732a77bd
mohammed-aabed <mohmmed.aabed@gmail.com>
2022-06-29 12:56:15 +0300

    make the header sticky

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+//   Sticky Header
+const sectionHeroEl = document.querySelector('.section-hero');
+const observer = new IntersectionObserver(
+  function (entries) {
+    const entry = entries[0];
+    if (!entry.isIntersecting) {
+      document.body.classList.add('sticky');
+    } else {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+observer.observe(sectionHeroEl);
